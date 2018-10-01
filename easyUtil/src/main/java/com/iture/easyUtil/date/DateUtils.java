07051662d93fb5937b6e9ab60e4a84d92ef4a0f6
iture <1011699225@qq.com>
2018-10-01 23:41:33 +0800

    日期

--- easyUtil/src/main/java/com/iture/easyUtil/date/DateUtils.java
+++ easyUtil/src/main/java/com/iture/easyUtil/date/DateUtils.java
@@ -1,5 +1,6 @@
 package com.iture.easyUtil.date;
 
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
@@ -17,12 +18,28 @@ public class DateUtils {
 		 c.add(Calendar.DAY_OF_YEAR, day);
 		 return c.getTime();
 	}
-	public static int getYyyymmdd(Date date) {
+	public static String formatDate(Date date,String format) {
+		 SimpleDateFormat sdf=new SimpleDateFormat(format);
+		 return sdf.format(date);
+	}
+	public static int getYyyyMMdd(Date date) {
 		 SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMdd");
 		 return Integer.parseInt(sdf.format(date));
 	}
-	public static int getYyyymmdd(long ms) {
+	public static int getYyyyMMdd(long ms) {
 		 SimpleDateFormat sdf=new SimpleDateFormat("yyyyMMdd");
 		 return Integer.parseInt(sdf.format(ms));
+	}
+	public static Date parseDay(String dateStr,String format) {
+		 SimpleDateFormat sdf=new SimpleDateFormat(format);
+		 try {
+			Date date = sdf.parse(dateStr);
+			return date;
+		} catch (ParseException e) {
+			throw new RuntimeException(e);
+		}
+	}
+	public static Date parseYyyyMMdd(String yyyyMMdd) {
+		 return parseDay(yyyyMMdd,"yyyyMMdd");
 	}
 } 
