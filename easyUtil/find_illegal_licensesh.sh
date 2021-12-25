2734a0b36a67c3d426dc6eb7e274f7b430ea3827
iture123 <1011699225@qq.com>
2021-12-25 18:11:38 +0800

    Update find_illegal_licensesh.sh

--- easyUtil/find_illegal_licensesh.sh
+++ easyUtil/find_illegal_licensesh.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-files=`find Linkis -type f -name "*.java" -o -name "*.xml" -o -name "*.scala"  -o -name "*.sh" -o -name "*.js" -o -name "*.scss"  -o -name "*.properties"|grep -v "\\.idea" |grep -v "\\.git"`
+files=`find -type f -name "*.java" -o -name "*.xml" -o -name "*.scala"  -o -name "*.sh" -o -name "*.js" -o -name "*.scss"  -o -name "*.properties"|grep -v "\\.idea" |grep -v "\\.git"`
 for file in $files;do
         line_cnt=`cat -n $file | grep "http://www.apache.org/licenses/LICENSE-2.0" | awk '{print $1}'|head -1`
 	if [[ ! -n "$line_cnt" || $line_cnt -gt 10 || $line_cnt -lt 8 ]];then
